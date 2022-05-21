c967ad9ded3b80d1c15c3e3a0ade4fbb088e79cf
gilang_gans <gilangsos99@gmail.com>
2022-05-21 21:44:19 +0700

    Tugas 27 js
    TUgas ini untuk niomic

---
+++ javascript/tugas27.js
@@ -0,0 +1,13 @@
+var kata = "No ";
+var kata2 = "Rumah saya ";
+var number = 30;
+var kata3 = " A";
+var string = kata.concat(kata2, number, kata3);
+console.log(string);
+
+var kata = "No ";
+var kata2 = "Rumah saya ";
+var number = 30;
+var kata3 = " A";
+var string = kata.concat(kata2, number, kata3);
+console.log(string);
